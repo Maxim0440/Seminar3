da36eee4f170810d76677d7f6971e6b4ace41e5c
Maxim Kornilov <M.kornilov.flights@gmail.com>
2023-02-11 15:12:32 +0300

    задача на нахождение квадратов чисел

---
+++ Task3/Program.cs
@@ -0,0 +1,18 @@
+﻿// // //Напишите программу, которая 
+// принимает на вход число (N) и 
+// выдаёт таблицу квадратов чисел 
+// // от 1 до N.
+
+// // 5 -> 1, 4, 9, 16, 25.
+// // 2 -> 1,4
+
+
+int num = new Random().Next(1,21);
+System.Console.WriteLine($"num = {num}");
+int i = 1;
+while (i <= num)
+
+{
+    System.Console.WriteLine(Math.Pow(i, 2));
+i++;
+}
