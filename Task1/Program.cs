a8a4d82bd4238ad95956880435a263e47bb35e0e
Maxim Kornilov <M.kornilov.flights@gmail.com>
2023-02-11 10:54:09 +0300

    адача на нахождение диапазона координат точек в разных четвертях

---
+++ Task1/Program.cs
@@ -0,0 +1,26 @@
+﻿// // напишите программу которая по заданному 
+// номеру четверти показывает диапазон
+//  координат точек в этой четверти
+
+int num = new Random().Next(1,5);
+System.Console.WriteLine(num);
+
+if (num == 1)
+{
+    System.Console.WriteLine("x от 0 до +&, y от 0 до +&");
+}
+
+else if (num == 2)
+{
+    System.Console.WriteLine("x от 0 до -&, y от 0 до +&");
+}
+else if (num == 3)
+{
+System.Console.WriteLine("x от 0 до -&, y от 0 до -&");
+}
+else if (num == 4)
+{
+    System.Console.WriteLine("x от 0 до +&, y от 0 до -&");
+}
+else
+System.Console.WriteLine("нет такой четверти");
